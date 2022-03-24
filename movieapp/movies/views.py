04d3257c96362e060a8e8dbf68d8be44f72d9c35
duyguadadivle <duyguadadivle@gmail.com>
2022-03-24 14:53:14 +0300

    dynamic homepage

--- movieapp/movies/views.py
+++ movieapp/movies/views.py
@@ -1,10 +1,56 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from tomlkit import date
+from datetime import date
 
 # Create your views here.
 
+data = {
+    "movies": [
+        {
+            "title": "film adı 1",
+            "description": "film açıklama 1",
+            "imageUrl": "m1.jpg",
+            "slug": "film-adi-1",
+            "language": "english",
+            "date": date(2023,5,5)
+        },
+        {
+            "title": "film adı 2",
+            "description": "film açıklama 2",
+            "imageUrl": "m2.jpg",
+            "slug": "film-adi-2",
+            "language": "english",
+            "date": date(2023,5,6)
+        },
+        {
+            "title": "film adı 3",
+            "description": "film açıklama 3",
+            "imageUrl": "m3.jpg",
+            "slug": "film-adi-3",
+            "language": "english",
+            "date": date(2023,5,7)
+        },
+        {
+            "title": "film adı 4",
+            "description": "film açıklama 4",
+            "imageUrl": "m4.jpg",
+            "slug": "film-adi-4",
+            "language": "english",
+            "date": date(2023,5,8)
+        },
+    ],
+    "slider": [],
+}
+
+
 def index(request):
-    return render(request, 'index.html')
+    movies = data["movies"]
+    
+    return render(request, 'index.html',{
+        "movies": movies
+
+    })
 
 
 def movies(request):
