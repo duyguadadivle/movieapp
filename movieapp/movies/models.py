631384762e7634d580f7de5e0134788ca8360258
duyguadadivle <duyguadadivle@gmail.com>
2022-04-27 15:03:12 +0300

    admin config

--- movieapp/movies/models.py
+++ movieapp/movies/models.py
@@ -31,14 +31,28 @@ class Person(models.Model):
         ('4','Senarist'),
     )
 
-    first_name = CharField(max_length=50)
-    last_name = CharField(max_length=50)
-    biography = CharField(max_length=3000)
+    first_name = CharField("Başlık",max_length=50)
+    last_name = CharField("Özet",max_length=50)
+    biography = CharField("Resim",max_length=3000)
     image_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
-    gender = models.CharField(max_length=1, choices=genders)
-    duty_type = models.CharField(max_length=1, choices=duty_types)
+    gender = models.CharField("cinsiyet",max_length=1, choices=genders)
+    duty_type = models.CharField("görev",max_length=1, choices=duty_types)
     contact = models.OneToOneField(Contact, on_delete=models.CASCADE, null=True, blank=True)
+
+    @property
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+    full_name.fget.short_description = "ad soyad"
+
+
+    class Meta:
+        verbose_name = "Kişi"
+        verbose_name_plural = "Kişiler"
+
+
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.duty_types[int(self.duty_type)-1][1]})" 
@@ -54,6 +68,10 @@ class Movie(models.Model):
     language = models.CharField(max_length=100)
     people = models.ManyToManyField(Person)
     genres = models.ManyToManyField(Genre)
+
+    class Meta:
+        verbose_name = "Film"
+        verbose_name_plural = "Fimler"
 
     def __str__(self):
         return self.title
