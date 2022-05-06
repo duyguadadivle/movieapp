24ec4e34c5199076195f9d13594bb3eab9c0e11c
duyguadadivle <duyguadadivle@gmail.com>
2022-05-06 12:57:21 +0300

    admin.py for comment

--- movieapp/movies/admin.py
+++ movieapp/movies/admin.py
@@ -1,22 +1,31 @@
 from django.contrib import admin
 
-from movies.models import Contact, Genre, Movie, Person, Video
+from movies.models import Contact, Genre, Movie, Person, Video, Comment
 
 
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ('title', 'is_activate', 'is_home')
+    list_display = ('title', 'is_active', 'is_home',)
     prepopulated_fields = {'slug': ('title',)}
-    list_filter = ('genres', 'language', 'is_activate', 'is_home')
+    list_filter = ('genres', 'language', 'is_active', 'is_home',)
     search_fields = ('title', 'description',)
 
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('full_name', 'gender', 'duty_type')
+    list_filter = ('gender','duty_type')
     search_fields = ('first_name', 'last_name')
 
 
-admin.site.register(Movie)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('full_name', 'movie',)
+    list_filter = ('full_name', 'movie',)
+    search_fields = ('movie__title', 'text')
+
+
+
+admin.site.register(Movie, MovieAdmin)
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Contact)
 admin.site.register(Genre)
 admin.site.register(Video)
+admin.site.register(Comment, CommentAdmin)
